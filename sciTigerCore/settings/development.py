b7a6de2134a8fc6d65ba5180c118302b86ea05a1
alanFu <alanfu@alanFus-Mac-mini.local>
2025-06-22 22:36:32 +0800

    移除django-debug-toolbar

--- sciTigerCore/settings/development.py
+++ sciTigerCore/settings/development.py
@@ -66,9 +66,3 @@ LOGGING = {
         },
     },
 }
-
-# Django Debug Toolbar配置
-if DEBUG:
-    INSTALLED_APPS += ['debug_toolbar']
-    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
-    INTERNAL_IPS = ['127.0.0.1']
